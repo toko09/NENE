bd78a04dd2fd5eff17d65033d9bec3f20176d2eb
Tornike Vakhtangidze <tornikevakhtangidze@gmail.com>
2024-11-25 23:02:16 +0400

    Rotated but GIF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+   
+  
 }
